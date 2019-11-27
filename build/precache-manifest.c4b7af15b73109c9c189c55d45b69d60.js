f550c746903aeb37a5dfb6320df6a51d1d06dd1e
benelan <benelan@yahoo.com>
2019-11-27 09:55:58 -0800

    beta build

---
+++ build/precache-manifest.c4b7af15b73109c9c189c55d45b69d60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344acda4fdf3cd4ac94c856cb78fd1bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32c90bfd275e5c8312c0",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "52ae0c2833d9141a4853",
+    "url": "/static/css/main.d720f70d.chunk.css"
+  },
+  {
+    "revision": "32c90bfd275e5c8312c0",
+    "url": "/static/js/2.f6b33dc4.chunk.js"
+  },
+  {
+    "revision": "52ae0c2833d9141a4853",
+    "url": "/static/js/main.c637b7fd.chunk.js"
+  },
+  {
+    "revision": "d441cad7841b0ee692fe",
+    "url": "/static/js/runtime-main.5f0ced77.js"
+  },
+  {
+    "revision": "0902b57bf68a956c8b3191aab0d1ecd1",
+    "url": "/static/media/logo.0902b57b.png"
+  }
+]);
